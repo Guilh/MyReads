d31d424bf967de7571fb988c92f3ff585bc04a08
Guil Hernandez <g.hernandez07@gmail.com>
2017-08-13 16:21:23 -0400

    refactor search

--- src/components/Book.js
+++ src/components/Book.js
@@ -24,14 +24,14 @@ class Book extends Component {
 
   render() {
     const { book } = this.props
-
+    console.log(this.state.shelf)
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select value={this.state.shelf} onChange={e => this.updateValue(e.target.value)}>
+              <select value={this.state.shelf || 'none'} onChange={e => this.updateValue(e.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading" >Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,41 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
+import * as BooksAPI from '../BooksAPI'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import Book from './Book'
 
-const SearchBooks = (props) => {
+class SearchBooks extends Component {
 
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link className="close-search" to="/">Close</Link>
-        <div className="search-books-input-wrapper">
-          <input
-            type="text"
-            placeholder="Search by title or author"
-            onChange={e => props.search(e.target.value)}
-          />
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    updateShelf: PropTypes.func.isRequired
+  }
+
+  state = {
+    searchedBooks: []
+  }
+
+  updateResults(searchResults) {
+     const inMyReads = this.props.books.map(b => b.id)
+     searchResults.forEach(book => {
+       inMyReads.includes(book.id) && (
+         book.shelf = this.props.books.filter(b => b.id === book.id)[0].shelf
+       )
+     })
+     this.setState({ searchedBooks: searchResults })
+  }
+
+  search = (query) => {
+    BooksAPI.search(query).then(books => this.updateResults(books))
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={e => this.search(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.searchedBooks.map(book => (
+              <Book
+                book={book}
+                updateShelf={this.props.updateShelf}
+                key={book.id}
+              />
+            ))}
+          </ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {props.books.map(book => (
-            <Book
-              book={book}
-              updateShelf={props.updateShelf}
-              key={book.id}
-            />
-          ))}
-        </ol>
-      </div>
-    </div>
-  )
-}
-
-SearchBooks.propTypes = {
-  search: PropTypes.func.isRequired,
-  books: PropTypes.array.isRequired,
-  updateShelf: PropTypes.func.isRequired
+    )
+  }
 }
 
 export default SearchBooks
